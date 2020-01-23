78e99ce20111cb7b8afd608ff899c0854aa81e91
Hitesh Yadav <hhiteshyadav@gmail.com>
2020-01-23 19:01:56 +0530

    Create 3-constants.go

---
+++ learn/intro/3-constants.go
@@ -0,0 +1,15 @@
+// https://golang.org/ref/spec#Constants
+
+package main
+
+import "fmt"
+
+const (
+	Pi    = 3.14
+)
+
+func main(){
+  const name = "Hitesh";
+  fmt.Println(name)
+  fmt.Println(Pi)
+}
