083d8fe0f8d3a9ac047204332849034affa7d25a
Hitesh Yadav <hhiteshyadav@gmail.com>
2020-01-23 18:21:12 +0530

    Update 2-variables-and-inferred-typing.go

--- learn/intro/2-variables-and-inferred-typing.go
+++ learn/intro/2-variables-and-inferred-typing.go
@@ -1,1 +1,20 @@
+// http://www.golangbootcamp.com/book/_single-page#sec-variables
 
+package main
+
+import "fmt"
+
+func main(){
+  var name = "Hitesh";
+  fmt.Printf(name);
+  
+  age := 21;
+  // fmt.Printf(age) would not work
+  fmt.Printf("%d", age);
+  
+  var(
+    details string = "None";
+  );
+  fmt.Printf(details);
+}
+  
