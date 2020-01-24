6f9bf432f636a4c75c40c24febf5c780ada6463a
Hitesh Yadav <hhiteshyadav@gmail.com>
2020-01-24 11:35:08 +0530

    Create 5-functions-1.cpp

---
+++ learn/basics/5-functions-1.cpp
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "fmt"
+)
+
+func add(x int, y int) int {
+  return x + y
+}
+
+/**
+func add(x, y int) int {
+  return x + y
+}
+**/
+
+func main(){
+  fmt.Println(add(2,3))
+}
+
+/**
+// Output
+
+5
+
+**/
