9434893fb3d5747971d7ff92b8b8147864e1311f
Hitesh Yadav <hhiteshyadav@gmail.com>
2020-02-15 15:32:07 +0530

    Create 15-slices-range.go

---
+++ learn/basics/15-slices-range.go
@@ -0,0 +1,18 @@
+// The range form of the for loop iterates over a slice or map.
+
+// When ranging over a slice, two values are returned for each iteration. 
+// The first is the index, and the second is a copy of the element at that index. 
+
+// https://tour.golang.org/moretypes/16
+
+package main
+
+import "fmt"
+
+var pow = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+func main() {
+  for index, value := range pow {
+    fmt.Println(index, value)
+  }
+}
