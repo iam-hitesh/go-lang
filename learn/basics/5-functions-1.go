3cebe85adb66c6937e2d0dcc69071586ca839498
Hitesh Yadav <hhiteshyadav@gmail.com>
2020-02-15 15:56:18 +0530

    Update 5-functions-1.go

--- learn/basics/5-functions-1.go
+++ learn/basics/5-functions-1.go
@@ -8,15 +8,15 @@ func add(x int, y int) int {
   return x + y
 }
 
-/**
-func add(x, y int) int {
-  return x + y
+func muliplication(a, b int) int {
+  return a * b;
 }
-**/
 
-func main(){
-  fmt.Println(add(2,3))
+func main() {
+  fmt.Println(add(2, 3))
+  fmt.Println(muliplication(2, 3))
 }
+
 
 /**
 // Output
