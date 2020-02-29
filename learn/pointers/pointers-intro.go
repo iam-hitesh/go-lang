1bc8a448a7eee0652c6914fa2f7148745411c3bc
Hitesh Yadav <hhiteshyadav@gmail.com>
2020-02-29 12:29:24 +0530

    Update pointers-intro.go

--- learn/pointers/pointers-intro.go
+++ learn/pointers/pointers-intro.go
@@ -3,16 +3,16 @@ package main
 import "fmt"
 
 func changeVal(ival *int, value int) {
-	*ival = 0
+	*ival = value
 }
 
 func main(){
 	ival := 3
 	fmt.Println("initial:", ival)
 	
-	changeVal(&ival, 0)
+	changeVal(&ival, 20)
 	fmt.Println("After value update:", ival)
 	
-	changeVal(&ival, 1)
-	fmt.Println("After chnage:", ival)
+	changeVal(&ival, 10)
+	fmt.Println("After change:", ival)
 }
